1b097960d56f6ec3829aa73855e7f9528574f9d7
ontaras <ontaras@yahoo.com>
2024-06-03 12:29:57 +0100

    constructor test added

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -3,24 +3,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
-
     @Test
-    void getName() {
-    }
-
-    @Test
-    void getSpeed() {
-    }
-
-    @Test
-    void getDistance() {
-    }
-
-    @Test
-    void move() {
-    }
-
-    @Test
-    void getRandomDouble() {
+    void constructor_NullAsNameParam_ThrownIllegalArgumentException() {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Horse(null, 0, 0);
+                }
+        );
+        assertEquals("Name cannot be null.", exception.getMessage());
     }
 }
